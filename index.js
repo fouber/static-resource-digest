09665cb7e7ea722c44ba5e3eaafce950a69953fa
fouber <fouber.NET@gmail.com>
2014-10-31 01:40:33 +0800

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
 
 fis.cli.help.commands = [ 'release', 'server' ];
 
+// extension map
 fis.config.set('roadmap.ext', {
     less: 'css',
     sass: 'css',
@@ -12,24 +13,49 @@ fis.config.set('roadmap.ext', {
     styl: 'css',
     stylus: 'css',
     coffee: 'js',
-    ts: 'js',
     md: 'html',
     jade: 'html',
-    ejs: 'html',
-    handlebars: 'html'
+    ejs: 'js',
+    handlebars: 'js'
 });
 
-fis.config.set('project.fileType.text', 'ts');
-
-fis.config.set('modules.parse', {
+// plugins
+fis.config.set('modules.parser', {
     less: 'less',
     sass: 'sass',
     scss: 'sass',
     styl: 'stylus',
     stylus: 'stylus',
     coffee: 'coffee-script',
-    ts: 'ts',
     md: 'marked',
     jade: 'jade',
-    ejs: 'ejs'
+    ejs: 'ejs',
+    handlebars: 'handlebars'
 });
+
+// plugin settings
+fis.config.set('modules.optimizer.html', 'html-minifier');
+fis.config.set('settings.optimizer.png-compressor.type', 'pngquant');
+fis.config.set('settings.optimizer.html-minifier', {
+    // fis直接将此配置传递给html-minfier模块
+    // 因此相关配置项请直接参阅html-minifier文档
+    removeComments: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    removeAttributeQuotes: true
+});
+
+fis.config.set('roadmap.path', [
+    {
+        reg: '*.ejs',
+        release: false
+    },
+    {
+        reg: '*.handlebars',
+        release: false
+    },
+    {
+        reg: /\/_[^\/]+$/,
+        release: false
+    }
+]);
